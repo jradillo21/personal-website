f19a7cc01bebc42ecba1d022debe7829cde71c90
jradillo21 <javierradillo21@gmail.com>
2019-03-13 19:38:01 -0600

    updated portfolio

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+
+	console.log("console test");
+
+	/*
+document.write ("console test");
+
+alert("alert test");
+
+confirm("confirm test");
+*/
+
+var box1 = document.getElementById("pic2")
+
+box1.innerHTML="<img src='http://placekitten.com/200/300'/>"
+
+
+
+
+
+
+
+
+
+		
